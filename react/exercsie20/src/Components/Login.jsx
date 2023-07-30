c8da18ec79cc5da4e2e2d9e383bcbb2d7397bc66
ibrabarry23 <66130926+ibrabarry23@users.noreply.github.com>
2023-07-30 16:22:33 +0200

    exe

---
+++ react/exercsie20/src/Components/Login.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+function Login({ onLog }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const passwordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const loginClick = () => {
+    onLog({ username, password });
+  };
+  const isDisabled = username === "" || password === "";
+  
+  const resset =()=>{
+    setUsername("");
+    setPassword("");
+  }
+
+  return (
+    <>
+      <label>Username:</label>
+      <input type="text" value={username} onChange={userChange} placeholder="Username" />
+
+      <label>Password:</label>
+      <input type="password" value={password} onChange={passwordChange} placeholder="Password" />
+
+      <button onClick={loginClick} disabled={isDisabled}>
+        Login
+      </button>
+      <button onClick={resset}>Reset</button>
+    </>
+  );
+}
+export default Login;
