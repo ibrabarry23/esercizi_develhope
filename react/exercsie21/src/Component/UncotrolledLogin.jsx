f539e46844eba6f8c0c0bd4382b07a3806f75b33
ibrabarry23 <66130926+ibrabarry23@users.noreply.github.com>
2023-07-30 16:45:48 +0200

    exe

---
+++ react/exercsie21/src/Component/UncotrolledLogin.jsx
@@ -0,0 +1,27 @@
+function UncotrolledLogin () {
+    const handleLog = ( event )=>{
+        event.preventDefault();
+
+        const username = event.target.elements.username.value ;
+        const password = event.target.elements.password.value ;
+        console.log({
+            username: username,
+            password:password
+        });
+    };
+    return(
+        <>
+        <form onSubmit={handleLog}>
+            <label>Username</label>
+            <input type="text" name="username" />
+
+            <label>Password</label>
+            <input type="password" name="password"  />
+
+            <button type="submit">Login</button>
+
+        </form>
+        </>
+    )
+}
+export default UncotrolledLogin
