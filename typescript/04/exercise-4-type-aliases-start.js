cf14d9dabd11fde9f809ac595285eeb733be02ae
ibrabarry23 <66130926+ibrabarry23@users.noreply.github.com>
2023-08-31 13:51:16 +0200

    exe

---
+++ typescript/04/exercise-4-type-aliases-start.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var country1 = "Nigeria";
+var country2 = "Italy";
+var country3 = "China";
