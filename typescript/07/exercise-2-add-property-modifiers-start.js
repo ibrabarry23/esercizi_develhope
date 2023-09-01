6ac25e3b5ce4cdcb0b4cc83608c61597899f6d30
ibrabarry23 <66130926+ibrabarry23@users.noreply.github.com>
2023-09-01 13:00:53 +0200

    exe

---
+++ typescript/07/exercise-2-add-property-modifiers-start.js
@@ -0,0 +1,26 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var countryA = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+var countryB = {
+    name: "Italy",
+    code: "IT"
+};
+var countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795,
+};
+var currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
+// currencyData.name = "Gold";
+currencyData.code = "GGG";
