da1dcb919171d4d501208c8ba72250093f20e4e1
ibrabarry23 <66130926+ibrabarry23@users.noreply.github.com>
2023-09-01 12:19:00 +0200

    exe

---
+++ typescript/06/exercise-1-define-object-types-start.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add a type annotation to this variable that describes the object it contains.
+var countryPopulation = {
+    code: "NZ",
+    population: 5135300,
+};
+var countryData = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+var currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
