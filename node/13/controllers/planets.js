86677f5b82b7d843fb3277db2a79ee5fa09b9336
ibrabarry23 <ibrabarry014@gmail.com>
2023-10-10 15:19:14 +0200

    exe

--- node/13/controllers/planets.js
+++ node/13/controllers/planets.js
@@ -1,18 +1,75 @@
-const express = require('express');
-const { json } = express;
-const { getAll, getOneById, create, updateById, deleteById } = require('./controllers/planets');
+const Joi = require('joi');
+const pgPromise = require('pg-promise');
 
-const app = express();
+const db = pgPromise()("postgres://postgres:postgres@localhost:5432/postgres");
 
+const planetSchema = Joi.object({
+    id: Joi.number().integer().required(),
+    name: Joi.string().required(),
+});
 
-app.use(json());
+const getAll = async (req, res) => {
+    try {
+        const planets = await db.many('SELECT * FROM planets;');
+        res.status(200).json(planets);
+    } catch (error) {
+        res.status(500).json({ msg: 'Internal Server Error' });
+    }
+};
 
-const PORT = 3000;
+const getOneById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const planet = await db.one('SELECT * FROM planets WHERE id =$1;', id);
+        res.status(200).json(planet);
+    } catch (error) {
+        res.status(404).json({ msg: 'Planet not found' });
+    }
+};
 
-app.get('/api/planets-controller', getAll);
-app.get('/api/planets-controller/:id', getOneById);
-app.post('/api/planets-controller', create);
-app.put('/api/planets-controller/:id', updateById);
-app.delete('/api/planets-controller/:id', deleteById);
+const create = async (req, res) => {
+    try {
+        const { id, name } = req.body;
+        const newPlanet = { id, name };
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+        const validatedNewPlanet = planetSchema.validate(newPlanet);
+
+        if (validatedNewPlanet.error) {
+            res.status(400).json({ msg: validatedNewPlanet.error });
+        } else {
+            await db.none('INSERT INTO planets (id, name) VALUES ($1, $2)', [id, name]);
+            res.status(201).json({ msg: 'The planet was created' });
+        }
+    } catch (error) {
+        res.status(500).json({ msg: 'Internal Server Error' });
+    }
+};
+
+const updateById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        await db.none('UPDATE planets SET name=$1 WHERE id=$2', [name, id]);
+        res.status(200).json({ msg: 'The planet was updated' });
+    } catch (error) {
+        res.status(500).json({ msg: 'Internal Server Error' });
+    }
+};
+
+const deleteById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await db.none('DELETE FROM planets WHERE id=$1', id);
+        res.status(200).json({ msg: 'The planet was deleted' });
+    } catch (error) {
+        res.status(500).json({ msg: 'Internal Server Error' });
+    }
+};
+
+module.exports = {
+    getAll,
+    getOneById,
+    create,
+    updateById,
+    deleteById,
+};
